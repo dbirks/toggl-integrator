0f7f91e5ad1508bc0c7486908a4ba4d3c2a2e4bb
David Birks <david@birks.dev>
2022-09-05 13:43:31 -0400

    Refactor into a getTogglTimeData func

--- cmd/toggl.go
+++ cmd/toggl.go
@@ -36,72 +36,58 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+
 	Run: func(cmd *cobra.Command, args []string) {
-		// fmt.Println("toggl called")
-		// getTogglTimes()
-		togglUsername := os.Getenv("TOGGL_USERNAME")
-		togglPassword := os.Getenv("TOGGL_PASSWORD")
-		// println(togglUsername)
-		// println(togglPassword)
 
-		req, err := http.NewRequest("GET",
-			"https://api.track.toggl.com/api/v9/me/time_entries", nil)
-		if err != nil {
-			print(err)
-		}
-		req.Header.Set("Content-Type", "application/json; charset=utf-8")
-		req.SetBasicAuth(togglUsername, togglPassword)
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			print(err)
-		}
-
-		defer resp.Body.Close()
-		togglResponse, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			print(err)
-		}
-
-		// fmt.Print(string(body))
-
-		type TimeEntry struct {
-			Start       time.Time `json:"start"`
-			Stop        time.Time `json:"stop"`
-			Description string    `json:"description"`
-			Duration    int       `json:"duration"`
-		}
+		togglTimeData := getTogglTimeData()
 
 		var togglTimeEntries []TimeEntry
 
-		// err := json.Unmarshal([]byte(body), &togglApiResponse)
-		json.Unmarshal([]byte(togglResponse), &togglTimeEntries)
+		err := json.Unmarshal([]byte(togglTimeData), &togglTimeEntries)
 
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
+		if err != nil {
+			fmt.Println(err)
+		}
 
 		fmt.Println(togglTimeEntries)
 
 	},
 }
 
-func init() {
-	getCmd.AddCommand(togglCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// togglCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// togglCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+type TimeEntry struct {
+	Start       time.Time `json:"start"`
+	Stop        time.Time `json:"stop"`
+	Description string    `json:"description"`
+	Duration    int       `json:"duration"`
 }
 
-func getTogglTimes() {
-	fmt.Println("Hi mom")
+func init() {
+	getCmd.AddCommand(togglCmd)
+}
 
+func getTogglTimeData() []byte {
+	togglUsername := os.Getenv("TOGGL_USERNAME")
+	togglPassword := os.Getenv("TOGGL_PASSWORD")
+
+	req, err := http.NewRequest("GET",
+		"https://api.track.toggl.com/api/v9/me/time_entries", nil)
+	if err != nil {
+		print(err)
+	}
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.SetBasicAuth(togglUsername, togglPassword)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		print(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		print(err)
+	}
+
+	return body
 }
